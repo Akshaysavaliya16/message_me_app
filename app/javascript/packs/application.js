711732326676fe5c9325f17c17d888b8656c9c12
Akshay Savaliya <savaliyaakshay@i.farm>
2021-08-17 11:23:42 +0530

    navigation with navigation parser and favivon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+
 
 Rails.start()
 Turbolinks.start()
